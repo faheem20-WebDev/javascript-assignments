03880c3f9ea3918d9ade343681f925016c6afa59
faheem20-WebDev <muhammadfaheem42004@gmai.com>
2025-11-10 23:51:52 +0500

    chapter02

---
+++ chapter02/question03.js
@@ -0,0 +1,2 @@
+var message="HelloWorld"
+alert(message);
---
+++ chapter02/question04.js
@@ -0,0 +1,6 @@
+var studentName="Muhammad Faheem Ahmed";
+alert(studentName);
+var studentAge="19 Years Old";
+alert(studentAge);
+var studentQualification="Certified Web Developer";
+alert(studentQualification);
