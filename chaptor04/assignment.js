e642ed2e4d0bcc82360f64b28ec0534ecfda347c
faheem20-WebDev <muhammadfaheem42004@gmai.com>
2025-11-11 23:52:11 +0500

    chapter04

---
+++ chaptor04/assignment.js
@@ -0,0 +1,12 @@
+// question no 01
+var name ,age,gender;
+
+
+//question no 02 
+var name,$price,customer_name,customerHeight,menLength05,Student-name,customer.name,customer height , men#length, 4age;
+
+
+// question NO 03 
+document.writeln("<h1> Rules for Naming Javascript variables </h1>");
+document.writeln("Variable Name can only be Numbers , $ and _ <br> For Example $Name , _name , or name <br> Variables Names are case senstive <br> Variables names  cannot be JS keywords");
+
