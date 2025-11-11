dddb77e0eabf4a2a99d9ebee04b5c3bf39603630
faheem20-WebDev <muhammadfaheem42004@gmai.com>
2025-11-11 22:59:16 +0500

    chapter03 chapter04

--- chapter02/question04.js
+++ chapter02/question04.js
@@ -4,3 +4,30 @@ var studentAge="19 Years Old";
 alert(studentAge);
 var studentQualification="Certified Web Developer";
 alert(studentQualification);
+
+
+// question 05
+var pizza="PIZZA \n PIZZ \n PIZ\n PI\n P";
+alert(pizza);
+
+
+
+// question 06 
+var email = "muhammadfaheem42004@gmail.com";
+alert("My email Address is " + email);
+
+
+
+//question 07 
+var book="A smarter way to Learn Javascript";
+alert("I am learn from the Book "+ book);
+
+
+//question 08
+var message = "Yeah !  I can write HTML content through Javascript";
+document.writeln(message);
+  
+
+// question 09 
+var script="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(script);
---
+++ chaptor03/assignment.js
@@ -0,0 +1,22 @@
+// question no 01 
+var age = 19;
+alert("I am " + age +" year old");
+
+
+// question no 02 
+var visits= 17;
+alert("You have visited this site "+visits +" times");
+
+
+// question No 03
+var birthYear=2006;
+document.write("\nMy birth Year is " +birthYear);
+document.write("\n Data type of my declared variable is number" );
+
+
+// question No 04 
+var visitorName= prompt("\n What is your name");
+var productTitle= prompt("\n Enter product Title");
+var quantity=prompt("\n Enter Quantity of product you want to order");
+
+document.write( visitorName + " has ordered  " +quantity  +productTitle+ " On XYZ Clothing Store");
